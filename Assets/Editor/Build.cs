caac62303473f97fd27a31aafc48a49c9c2ecc33
PubuduS <pubudusupun@gmail.com>
2022-02-08 01:04:11 -0600

    add network

--- Assets/Editor/Build.cs
+++ Assets/Editor/Build.cs
@@ -78,13 +78,13 @@ public class Build
 
         content = content.Replace("<AppxBundle>Auto</AppxBundle>",
             "<AppxBundle>Always</AppxBundle>\r\n" + //Force creating App Bundle
-            "    <GenerateAppInstallerFile>False</GenerateAppInstallerFile>\r\n" + //..and App Installer (for automatic update)
+            "    <GenerateAppInstallerFile>True</GenerateAppInstallerFile>\r\n" + //..and App Installer (for automatic update)
             "    <AppxAutoIncrementPackageRevision>True</AppxAutoIncrementPackageRevision>\r\n" + //doesn't matter because version is generate everytime
             "    <AppxBundlePlatforms>ARM64</AppxBundlePlatforms>\r\n" + //HoloLens uses ARM64
-            //"    <AppInstallerUpdateFrequency>1</AppInstallerUpdateFrequency>\r\n" + //auto-update enabled
-            //"    <AppInstallerCheckForUpdateFrequency>OnApplicationRun</AppInstallerCheckForUpdateFrequency>\r\n" + //try updating every App start
-            "    <AppxPackageDir>" + locationPathName + PlayerSettings.productName + "\\</AppxPackageDir>"); //network drive share where we keep our HoloLens App-Builds
-            //"    <AppInstallerUri>http://holo-buildslave/UWP/" + PlayerSettings.productName + "/</AppInstallerUri>"); //HTTP-File Server (just mirroring the Network Drive)
+            "    <AppInstallerUpdateFrequency>1</AppInstallerUpdateFrequency>\r\n" + //auto-update enabled
+            "    <AppInstallerCheckForUpdateFrequency>OnApplicationRun</AppInstallerCheckForUpdateFrequency>\r\n" + //try updating every App start
+            "    <AppxPackageDir>\\\\tsclient\\Holographic\\UWP\\" + PlayerSettings.productName + "\\</AppxPackageDir>\r\n" + //network drive share where we keep our HoloLens App-Builds
+            "    <AppInstallerUri>http://10.70.157.108/" + PlayerSettings.productName + "/</AppInstallerUri>"); //HTTP-File Server (just mirroring the Network Drive)
         File.WriteAllText(vcxPath, content);
     }
 
