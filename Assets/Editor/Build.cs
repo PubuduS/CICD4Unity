a9c287851315115519d2f8a9a1dce170b62ef0a7
PubuduS <pubudusupun@gmail.com>
2022-02-10 13:22:12 -0600

    Fix app installer file path

--- Assets/Editor/Build.cs
+++ Assets/Editor/Build.cs
@@ -85,7 +85,7 @@ public class Build
             "    <AppInstallerUpdateFrequency>1</AppInstallerUpdateFrequency>\r\n" + //auto-update enabled
             "    <AppInstallerCheckForUpdateFrequency>OnApplicationRun</AppInstallerCheckForUpdateFrequency>\r\n" + //try updating every App start
             "    <AppxPackageDir>" + locationPathName + PlayerSettings.productName + "\\</AppxPackageDir>\r\n" + //network drive share where we keep our HoloLens App-Builds
-            "    <AppInstallerUri>https://built-artifacts.s3.us-east-2.amazonaws.com/_MRTK_GUI.appinstaller" + "</AppInstallerUri>"); //HTTP-File Server (just mirroring the Network Drive)
+            "    <AppInstallerUri>https://built-artifacts.s3.us-east-2.amazonaws.com/" + "</AppInstallerUri>"); //HTTP-File Server (just mirroring the Network Drive)
         File.WriteAllText(vcxPath, content);
     }
 
